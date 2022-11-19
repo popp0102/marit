26c7ddac6249ee826921cb4ec657eb80bb0e2aed
jpoppler <popp0102@gmail.com>
2022-11-19 13:25:12 -0700

    legitimate test working
    spec for testing a NetworkDevice with a factory in rspec.

---
+++ spec/factories/network_devices.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :network_device do
+    mac_address { "some_mac_address" }
+    name  { "device 1" }
+  end
+end
+
--- spec/models/network_device_spec.rb
+++ spec/models/network_device_spec.rb
@@ -1,5 +1,9 @@
-RSpec.describe 'Network Device' do
-  it 'cannot have comments' do   #
-    expect(1).to eql(1)
+require 'rails_helper'
+
+RSpec.describe NetworkDevice, type: :model do
+  let(:network_device) { create(:network_device) }
+
+  it 'cannot have comments' do
+    expect(network_device).to be_valid
   end
 end
