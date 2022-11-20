a55affcef92d0fcdc0f4354d15302a1680bac021
jpoppler <jason@getnotion.com>
2022-11-20 13:00:13 -0700

    network devices view
    Table with interpolation working

--- app/controllers/network_devices_controller.rb
+++ app/controllers/network_devices_controller.rb
@@ -1,6 +1,5 @@
 class NetworkDevicesController < ApplicationController
   def index
     @network_devices = NetworkDevice.all
-    render json: @network_devices, each_serializer: NetworkDeviceSerializer, adapter: :json
   end
 end
