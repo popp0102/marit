d248cca9c5490d93efb90194053db79a8328cefd
jpoppler <jason@getnotion.com>
2022-11-20 13:58:05 -0700

    more framing
    add the ability to manually create a network device entry

--- app/controllers/network_devices_controller.rb
+++ app/controllers/network_devices_controller.rb
@@ -3,7 +3,18 @@ class NetworkDevicesController < ApplicationController
     @network_devices = NetworkDevice.all
   end
 
+  def new
+    @network_device = NetworkDevice.new
+    render :new
+  end
+
+  def create
+    @network_device = NetworkDevice.new(params.require(:network_device).permit(:mac_address, :name))
+    @network_device.save!
+    redirect_to network_devices_url
+  end
+
   def show
-    puts 'tracking'
+    puts "tracking: #{params}"
   end
 end
